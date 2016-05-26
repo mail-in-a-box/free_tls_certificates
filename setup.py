1489d97645c99de8951db38275e03add72c95237
Joshua Tauberer <jt@occams.info>
2016-05-26 14:04:47 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='free_tls_certificates',
-    version='0.1.3',
+    version='0.1.4',
     author=u'Joshua Tauberer',
     author_email=u'jt@occams.info',
     packages = find_packages(),
